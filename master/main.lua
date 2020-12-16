8f847f6483cbc861a45e1837a08688e2bb065e80
phansonloc1999 <phansonloc1999@gmail.com>
2020-12-16 09:36:36 +0700

    Added support for local lua debugger

--- master/main.lua
+++ master/main.lua
@@ -2,29 +2,31 @@
 -- Missed bullets are not destroyed
 -- Skill system is a mess -> Turning player into a state machine
 
+if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
+    require("lldebugger").start()
+end
 
-require 'globals'
-require 'assets'
+require "globals"
+require "assets"
 
-Game = require 'src.states.game'
-LevelUp = require 'src.states.levelUp'
-Pause = require 'src.states.pause'
-Menu = require 'src.states.menu'
+Game = require "src.states.game"
+LevelUp = require "src.states.levelUp"
+Pause = require "src.states.pause"
+Menu = require "src.states.menu"
 
 function love.load()
-  math.randomseed(os.time())
-  
-  love.mouse.setVisible(false)
-  
-  Gamestate.registerEvents()
-  Gamestate.switch(Menu)
+    math.randomseed(os.time())
+
+    love.mouse.setVisible(false)
+
+    Gamestate.registerEvents()
+    Gamestate.switch(Menu)
 end
 
 function love.update(dt)
-
 end
 
 function love.draw()
-  love.graphics.setColor(1, 1, 1)
-  love.graphics.print('FPS: '..tostring(love.timer.getFPS()))
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print("FPS: " .. tostring(love.timer.getFPS()))
 end
